b2f07286fd0dba0e454397ba19d7664defa6d02b
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-11-23 01:36:16 +0900

    feat: graph

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -2,26 +2,55 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from '../header/header';
 import { theme } from '../../common/styles/theme';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const Home = () => (
-  <HomeContainer>
-    <Header />
-    <OngoingSurvey>
-      <Title>진행중인 설문 </Title>
-      <Count>2</Count>
-      <Surveys>
-        <Survey>
-          <SurveyTitle>이색 콜라보 식품 구매 경험 및 인식</SurveyTitle>
-          <GraphContainer>
-            <Graph>
-              <HelpGraph />
-            </Graph>
-          </GraphContainer>
-        </Survey>
-      </Surveys>
-    </OngoingSurvey>
-  </HomeContainer>
-);
+const Home = () => {
+  const percentage = 66;
+
+  return (
+    <HomeContainer>
+      <Header />
+      <OngoingSurvey>
+        <Title>진행중인 설문 </Title>
+        <Count>2</Count>
+        <Surveys>
+          <Survey>
+            <SurveyTitle>이색 콜라보 식품 구매 경험 및 인식</SurveyTitle>
+            <GraphContainer>
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={buildStyles({
+                  // Rotation of path and trail, in number of turns (0-1)
+                  rotation: 0.25,
+
+                  // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                  strokeLinecap: 'butt',
+
+                  // Text size
+                  textSize: '16px',
+
+                  // How long animation takes to go from one percentage to another, in seconds
+                  pathTransitionDuration: 0.5,
+
+                  // Can specify path transition in more detail, or remove it entirely
+                  // pathTransition: 'none',
+
+                  // Colors
+                  pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+                  textColor: '#f88',
+                  trailColor: '#d6d6d6',
+                  backgroundColor: '#3e98c7',
+                })}
+              />
+            </GraphContainer>
+          </Survey>
+        </Surveys>
+      </OngoingSurvey>
+    </HomeContainer>
+  );
+};
 
 export default Home;
 
@@ -48,7 +77,10 @@ const SurveyTitle = styled.text`
   font-weight: ${theme.typography.semiBold};
 `;
 
-const GraphContainer = styled.div``;
+const GraphContainer = styled.div`
+  width: 128px;
+  height: 128px;
+`;
 
 const Graph = styled.div`
   width: 128px;
