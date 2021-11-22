8310d50eb6b7f32de27cd27460f01ddf20b7fd70
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-11-23 01:16:12 +0900

    feat: demon graph

---
+++ src/features/header/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import typoLogo from '../../common/images/typoLogo.png';
+import alarmImage from '../../common/images/alarm.png';
+
+const Header = () => (
+  <Head>
+    <Logo src={typoLogo} />
+    <Alarm onClick={() => console.log('alarm click')} />
+  </Head>
+);
+
+const Head = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 14px 16px;
+  border: 1px solid #f1f1f1;
+`;
+
+const Logo = styled.img`
+  width: 76.13px;
+  height: 20px;
+`;
+
+const Alarm = styled.button`
+  width: 20px;
+  height: 20px;
+  background-image: url(${alarmImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export default Header;
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
 import styled from 'styled-components';
+import Header from '../header/header';
+import { theme } from '../../common/styles/theme';
 
-const Home = () => <Text>Home</Text>;
+const Home = () => (
+  <HomeContainer>
+    <Header />
+    <OngoingSurvey>
+      <Title>진행중인 설문 </Title>
+      <Count>2</Count>
+      <Surveys>
+        <Survey>
+          <SurveyTitle>이색 콜라보 식품 구매 경험 및 인식</SurveyTitle>
+          <GraphContainer>
+            <Graph>
+              <HelpGraph />
+            </Graph>
+          </GraphContainer>
+        </Survey>
+      </Surveys>
+    </OngoingSurvey>
+  </HomeContainer>
+);
 
 export default Home;
 
-const Text = styled.text``;
+const HomeContainer = styled.section``;
+
+const OngoingSurvey = styled.section`
+  padding: 14px 16px;
+`;
+
+const Title = styled.text`
+  font-weight: ${theme.typography.medium};
+`;
+
+const Count = styled.text`
+  font-weight: ${theme.typography.medium};
+  color: ${theme.colors.orange};
+`;
+
+const Surveys = styled.section``;
+
+const Survey = styled.div``;
+
+const SurveyTitle = styled.text`
+  font-weight: ${theme.typography.semiBold};
+`;
+
+const GraphContainer = styled.div``;
+
+const Graph = styled.div`
+  width: 128px;
+  height: 128px;
+  background: linear-gradient(91.28deg, #f69738 -0.03%, #f4745f 99.97%);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HelpGraph = styled.div`
+  width: 108px;
+  height: 108px;
+  background: white;
+  border-radius: 50%;
+`;
+
+const GraphDes = styled.text``;
+
+const GraphCount = styled.text``;
