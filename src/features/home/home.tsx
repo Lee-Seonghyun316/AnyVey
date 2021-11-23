2142d2b57e0a33e167ddd57ecf99990a5056b5b5
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-11-23 19:31:45 +0900

    feat: 진행 중인 설문의 개별 설문 완료

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -10,28 +10,42 @@ import 'react-circular-progressbar/dist/styles.css';
 import './custom.css';
 
 const Home = () => {
-  const percentage = 82;
+  const respondent = 82;
+  const Recruitment = 100;
+  const percentage = (respondent / Recruitment) * 100;
 
   return (
     <HomeContainer>
       <Header />
       <OngoingSurvey>
-        <Title>진행중인 설문 </Title>
-        <Count>2</Count>
+        <TitleContainer>
+          <Title>진행중인 설문 </Title>
+          <Count>2</Count>
+        </TitleContainer>
         <Surveys>
           <Survey>
             <SurveyTitle>이색 콜라보 식품 구매 경험 및 인식</SurveyTitle>
             <GraphContainer>
-              <CircularProgressbarWithChildren
-                value={percentage}
-                styles={buildStyles({
-                  pathColor: `${theme.colors.orange}`,
-                  trailColor: '#F1F1F1',
-                })}
-              >
-                <GraphText>{percentage}%</GraphText>
-              </CircularProgressbarWithChildren>
+              <GraphSize>
+                <CircularProgressbarWithChildren
+                  value={percentage}
+                  styles={buildStyles({
+                    pathColor: `${theme.colors.orange}`,
+                    trailColor: '#F1F1F1',
+                  })}
+                >
+                  <GraphText>{percentage}%</GraphText>
+                </CircularProgressbarWithChildren>
+              </GraphSize>
+              <GraphDes>
+                <GraphDesTitle>응답현황</GraphDesTitle> <br /> {percentage}/
+                {Recruitment}
+              </GraphDes>
             </GraphContainer>
+            <SurveyDes>
+              <DeadLine>마감 2021.12.12</DeadLine>
+              <Gift>보상</Gift>
+            </SurveyDes>
           </Survey>
         </Surveys>
       </OngoingSurvey>
@@ -44,7 +58,11 @@ export default Home;
 const HomeContainer = styled.section``;
 
 const OngoingSurvey = styled.section`
-  padding: 14px 16px;
+  padding: 20px 16px;
+`;
+
+const TitleContainer = styled.div`
+  margin-bottom: 20px;
 `;
 
 const Title = styled.text`
@@ -60,16 +78,29 @@ const Count = styled.text`
 
 const Surveys = styled.section``;
 
-const Survey = styled.div``;
+const Survey = styled.div`
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 16px;
+  width: 254px;
+`;
 
-const SurveyTitle = styled.text`
+const SurveyTitle = styled.div`
   font-weight: ${theme.typography.semiBold};
   font-size: 18px;
 `;
 
 const GraphContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 30px 0;
+`;
+
+const GraphSize = styled.div`
   width: 128px;
   height: 128px;
+  margin-right: 16px;
 `;
 
 const GraphText = styled.div`
@@ -77,3 +108,21 @@ const GraphText = styled.div`
   font-weight: ${theme.typography.bold};
   font-size: 20px;
 `;
+
+const GraphDes = styled.div`
+  line-height: 16.34px;
+  font-size: 12px;
+`;
+
+const GraphDesTitle = styled.text`
+  color: #b4b4b4;
+`;
+
+const SurveyDes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: #b4b4b4;
+  font-size: 12px;
+`;
+const DeadLine = styled.div``;
+const Gift = styled.div``;
