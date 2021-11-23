3c4c63dd08d4cf3c7a7b407c7c77e9aa8458a29f
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-23 18:02:39 +0900

    feat: circular-progressbar

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from '../header/header';
 import { theme } from '../../common/styles/theme';
-import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import './custom.css';
 
 const Home = () => {
-  const percentage = 66;
+  const percentage = 82;
 
   return (
     <HomeContainer>
@@ -18,32 +22,15 @@ const Home = () => {
           <Survey>
             <SurveyTitle>이색 콜라보 식품 구매 경험 및 인식</SurveyTitle>
             <GraphContainer>
-              <CircularProgressbar
+              <CircularProgressbarWithChildren
                 value={percentage}
-                text={`${percentage}%`}
                 styles={buildStyles({
-                  // Rotation of path and trail, in number of turns (0-1)
-                  rotation: 0.25,
-
-                  // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                  strokeLinecap: 'butt',
-
-                  // Text size
-                  textSize: '16px',
-
-                  // How long animation takes to go from one percentage to another, in seconds
-                  pathTransitionDuration: 0.5,
-
-                  // Can specify path transition in more detail, or remove it entirely
-                  // pathTransition: 'none',
-
-                  // Colors
-                  pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
-                  textColor: '#f88',
-                  trailColor: '#d6d6d6',
-                  backgroundColor: '#3e98c7',
+                  pathColor: `${theme.colors.orange}`,
+                  trailColor: '#F1F1F1',
                 })}
-              />
+              >
+                <GraphText>{percentage}%</GraphText>
+              </CircularProgressbarWithChildren>
             </GraphContainer>
           </Survey>
         </Surveys>
@@ -62,11 +49,13 @@ const OngoingSurvey = styled.section`
 
 const Title = styled.text`
   font-weight: ${theme.typography.medium};
+  font-size: 16px;
 `;
 
 const Count = styled.text`
   font-weight: ${theme.typography.medium};
   color: ${theme.colors.orange};
+  font-size: 16px;
 `;
 
 const Surveys = styled.section``;
@@ -75,6 +64,7 @@ const Survey = styled.div``;
 
 const SurveyTitle = styled.text`
   font-weight: ${theme.typography.semiBold};
+  font-size: 18px;
 `;
 
 const GraphContainer = styled.div`
@@ -82,23 +72,8 @@ const GraphContainer = styled.div`
   height: 128px;
 `;
 
-const Graph = styled.div`
-  width: 128px;
-  height: 128px;
-  background: linear-gradient(91.28deg, #f69738 -0.03%, #f4745f 99.97%);
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const GraphText = styled.div`
+  color: ${theme.colors.orange};
+  font-weight: ${theme.typography.bold};
+  font-size: 20px;
 `;
-
-const HelpGraph = styled.div`
-  width: 108px;
-  height: 108px;
-  background: white;
-  border-radius: 50%;
-`;
-
-const GraphDes = styled.text``;
-
-const GraphCount = styled.text``;
