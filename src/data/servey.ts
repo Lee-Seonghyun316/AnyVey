2c355fa2f96ce46d625099372fd627be23f14bc0
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-24 12:28:21 +0900

    feat: 설문 데이터 분리

---
+++ src/data/servey.ts
@@ -0,0 +1,62 @@
+export const myServeys = [
+  {
+    id: 1,
+    title: '이색 콜라보 식품 구매 경험 및 인식',
+    respondent: 82,
+    recruitment: 100,
+    deadLine: '2021.12.12',
+    gift: 'etc',
+  },
+  {
+    id: 2,
+    title: '이색 콜라보 식품 구매 경험 및 인식',
+    respondent: 50,
+    recruitment: 100,
+    deadLine: '2021.12.12',
+    gift: 'coffee',
+  },
+  {
+    id: 3,
+    title: '이색 콜라보 식품 구매 경험 및 인식',
+    respondent: 50,
+    recruitment: 100,
+    deadLine: '2021.12.12',
+  },
+];
+
+export const interestSurveys = [
+  {
+    id: 1,
+    title: '제로(0kcal)음료에 대한 인식',
+    tag: '#20~30대  #일상',
+  },
+  {
+    id: 2,
+    title: '‘그린워싱’ 제품 사용 경험에 대한 설문조사',
+    tag: '#20~30대  #사회',
+    gift: 'etc',
+  },
+  {
+    id: 3,
+    title: '헬스케어 플랫폼 설문조사',
+    tag: '#20~30대  #IT',
+  },
+  {
+    id: 4,
+    title: '코로나19 이후 2030 우울감 정도',
+    tag: '#20~30대  #사회',
+    gift: 'coffee',
+  },
+  {
+    id: 5,
+    title: '코로나19 이후 2030 우울감 정도',
+    tag: '#20~30대  #일상',
+    gift: 'coffee',
+  },
+  {
+    id: 6,
+    title: '코로나19 이후 2030 우울감 정도',
+    tag: '#20~30대  #일상',
+    gift: 'coffee',
+  },
+];
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -9,75 +9,12 @@ import coin from '../../common/images/coin.png';
 import InterestSurvey from './interestSurvey';
 import BottomBar from '../bottomBar/bottomBar';
 import LogoutMySurvey from './logoutMySurvey';
-
+import { myServeys, interestSurveys } from '../../data/servey';
 const Home = () => {
-  const mySurveys = [
-    {
-      id: 1,
-      title: '이색 콜라보 식품 구매 경험 및 인식',
-      respondent: 82,
-      recruitment: 100,
-      deadLine: '2021.12.12',
-      gift: 'etc',
-    },
-    {
-      id: 2,
-      title: '이색 콜라보 식품 구매 경험 및 인식',
-      respondent: 50,
-      recruitment: 100,
-      deadLine: '2021.12.12',
-      gift: 'coffee',
-    },
-    {
-      id: 3,
-      title: '이색 콜라보 식품 구매 경험 및 인식',
-      respondent: 50,
-      recruitment: 100,
-      deadLine: '2021.12.12',
-    },
-  ];
-
-  const interests = [
-    {
-      id: 1,
-      title: '제로(0kcal)음료에 대한 인식',
-      tag: '#20~30대  #일상',
-    },
-    {
-      id: 2,
-      title: '‘그린워싱’ 제품 사용 경험에 대한 설문조사',
-      tag: '#20~30대  #사회',
-      gift: 'etc',
-    },
-    {
-      id: 3,
-      title: '헬스케어 플랫폼 설문조사',
-      tag: '#20~30대  #IT',
-    },
-    {
-      id: 4,
-      title: '코로나19 이후 2030 우울감 정도',
-      tag: '#20~30대  #사회',
-      gift: 'coffee',
-    },
-    {
-      id: 5,
-      title: '코로나19 이후 2030 우울감 정도',
-      tag: '#20~30대  #일상',
-      gift: 'coffee',
-    },
-    {
-      id: 6,
-      title: '코로나19 이후 2030 우울감 정도',
-      tag: '#20~30대  #일상',
-      gift: 'coffee',
-    },
-  ];
-  const interestsFirst = interests.slice(0, 4);
-  const interestsSecond = interests.slice(3);
+  const interestsFirst = interestSurveys.slice(0, 4);
+  const interestsSecond = interestSurveys.slice(3);
   const [moreToggle, setMoreToggle] = useState(false);
   const login = true;
-
   const handleMore = () => {
     setMoreToggle(!moreToggle);
   };
@@ -88,11 +25,11 @@ const Home = () => {
       <OngoingSurvey>
         <SectionTitleContainer>
           <SectionTitle>진행중인 설문 </SectionTitle>
-          {login && <Count>{mySurveys.length}</Count>}
+          {login && <Count>{myServeys.length}</Count>}
         </SectionTitleContainer>
         {login && (
           <MySurveys>
-            {mySurveys.map((mySurvey) => (
+            {myServeys.map((mySurvey) => (
               <MySurvey
                 key={mySurvey.id}
                 title={mySurvey.title}
