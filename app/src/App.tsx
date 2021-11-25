aa2bc849526c862b585316a06ef9ebbe4b44f98d
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2021-11-25 18:09:37 +0900

    error fix

---
+++ app/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Home from './features/home/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Reference from './features/reference/reference';
+import CreateSurvey from './features/createSurvey/createSurvey';
+
+const App = function () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reference" element={<Reference />} />
+        <Route path="createSurvey" element={<CreateSurvey />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
