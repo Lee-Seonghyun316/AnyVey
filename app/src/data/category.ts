95ae0c6812e815951d2c603de5ccd3345ec6cb04
Jiman Jeong <wlaks2317@gmail.com>
2021-11-26 20:09:44 +0900

    feat: checkbox form logic (#14)

--- app/src/data/category.ts
+++ app/src/data/category.ts
@@ -1,15 +1,53 @@
 export const fields = [
-  '사회',
-  '정치/경제',
-  '일상',
-  'IT',
-  '과학',
-  '예술',
-  '스포츠',
-  '무관',
+  {
+    id: 1,
+    value: 'society',
+    text: '사회',
+  },
+  {
+    id: 2,
+    value: 'economy',
+    text: '정치/경제',
+  },
+  {
+    id: 3,
+    value: 'life',
+    text: '일상',
+  },
+  {
+    id: 4,
+    value: 'it',
+    text: 'IT',
+  },
+  {
+    id: 5,
+    value: 'science',
+    text: '과학',
+  },
+  {
+    id: 6,
+    value: 'art',
+    text: '예술',
+  },
+  {
+    id: 7,
+    value: 'sport',
+    text: '스포츠',
+  },
+  {
+    id: 8,
+    value: 'irrelevant',
+    text: '무관',
+  },
 ];
 
-export const targetAges = ['10대', '20대', '30대', '40대', '50대', '전체'];
+export const targetAges = ['10대', '20대', '30대', '40대', '50대', '전체'].map(
+  (text, index) => ({
+    id: index + 1,
+    value: index !== 5 ? `${index + 1}0` : 'all',
+    text,
+  }),
+);
 
 export const targetGender = ['남성', '여성', '전체'];
 
--- app/src/features/createSurvey/createSurvey.tsx
+++ app/src/features/createSurvey/createSurvey.tsx
@@ -1,27 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Header from '../header/header';
 import { fields, gifts, targetAges, targetGender } from '../../data/category';
+import { Controller, useForm } from 'react-hook-form';
 
 const CreateSurvey = () => {
+  const { register, handleSubmit, control } = useForm();
+
   return (
     <Wrap>
       <Header headerStyle="next" />
       <Content>
-        <TitleInput placeholder="제목" />
+        <TitleInput {...register('title')} placeholder="제목" />
         <Contour />
-        <DesInput placeholder="세부 설명" />
+        <DesInput {...register('description')} placeholder="세부 설명" />
         <Contour />
         <Title>관련 분야</Title>
         <ButtonContainer>
-          {fields.map((item) => (
-            <Button key={item}>{item}</Button>
+          {fields.map(({ id, value, text }) => (
+            <Controller
+              key={id}
+              name="category"
+              control={control}
+              render={({ field }) => (
+                <Button
+                  onClick={(e) => {
+                    if (!field.value) {
+                      field.onChange([value]);
+                      return;
+                    }
+                    !field.value.includes(value)
+                      ? field.onChange(field.value.concat(value))
+                      : field.onChange(
+                          field.value.filter((item: string) => item !== value),
+                        );
+                  }}
+                  checked={field.value?.includes(value)}
+                >
+                  {text}
+                </Button>
+              )}
+            />
           ))}
         </ButtonContainer>
         <Title>타겟 연령대</Title>
         <ButtonContainer>
-          {targetAges.map((item) => (
-            <Button key={item}>{item}</Button>
+          {targetAges.map(({ id, value, text }) => (
+            <Controller
+              key={id}
+              name="target"
+              control={control}
+              render={({ field }) => (
+                <Button
+                  onClick={(e) => {
+                    if (!field.value) {
+                      field.onChange(value);
+                      return;
+                    }
+                    field.value === value
+                      ? field.onChange(null)
+                      : field.onChange(value);
+                  }}
+                  checked={field.value?.includes(value)}
+                >
+                  {text}
+                </Button>
+              )}
+            />
           ))}
         </ButtonContainer>
         <Title>타겟 성별</Title>
@@ -52,7 +97,9 @@ const CreateSurvey = () => {
       </ButtonContainer>
       <Bottom>
         <BottomText>취소</BottomText>
-        <BottomText>저장</BottomText>
+        <BottomText onClick={handleSubmit((data) => console.log(data))}>
+          저장
+        </BottomText>
       </Bottom>
     </Wrap>
   );
@@ -91,15 +138,26 @@ const ButtonContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
-const Button = styled.button`
+
+type ButtonProps = {
+  checked?: boolean;
+};
+const Button = styled.button<ButtonProps>`
   font-style: normal;
-  font-weight: normal;
+  ${({ checked, theme }) =>
+    checked ? theme.typography.bold : theme.typography.regular};
   font-size: 14px;
   line-height: 19px;
   background: #ffffff;
-  border: 1px solid #d6d6d6;
+  ${({ checked, theme }) => {
+    const color = checked ? theme.colors.orange : '#d6d6d6';
+
+    return css`
+      color: ${color};
+      border: 1px solid ${color};
+    `;
+  }};
   border-radius: 6px;
-  color: #d6d6d6;
   width: 78px;
   height: 29px;
   margin: 5px;
