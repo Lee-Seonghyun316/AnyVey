5806f584aafc20c6c996020d6ededaf7a8daf857
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-26 10:10:34 +0900

    style: add join ui/ux

--- app/src/common/components/ErrorMessage.tsx
+++ app/src/common/components/ErrorMessage.tsx
@@ -4,14 +4,18 @@ import styled from 'styled-components';
 interface Props {
   error: boolean;
   text: string;
+  width?: string;
 }
 
 interface PropsCSS {
   error: boolean;
+  width?: string;
 }
 
-const ErrorMessage: React.FC<Props> = ({ error, text }) => (
-  <IdErrorMessage error={error}>{error && text}</IdErrorMessage>
+const ErrorMessage: React.FC<Props> = ({ error, text, width }) => (
+  <IdErrorMessage error={error} width={width}>
+    {error && text}
+  </IdErrorMessage>
 );
 
 export default ErrorMessage;
@@ -25,4 +29,5 @@ const IdErrorMessage = styled.span<PropsCSS>`
   line-height: 23px;
   margin-bottom: 10px;
   height: 40px;
+  width: ${({ width }) => (width ? width : '100%')};
 `;
