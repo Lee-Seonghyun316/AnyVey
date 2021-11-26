440d01e22e006bfa595c3854bee606ccfbf64905
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-26 11:53:07 +0900

    style: add ReferenceDetail ux/ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,6 +9,7 @@ import Join from './features/join/join';
 import EmailAuth from './features/join/emailAuth/EmailAuth';
 import BasicInfo from './features/join/basicInformation/BasicInfo';
 import Search from './features/reference/search/Search';
+import ReferenceDetail from './features/reference/referenceDetail/ReferenceDetail';
 
 const App = function () {
   return (
@@ -22,6 +23,7 @@ const App = function () {
         <Route path="email-auth" element={<EmailAuth />} />
         <Route path="basic-info" element={<BasicInfo />} />
         <Route path="search" element={<Search />} />
+        <Route path="reference-detail" element={<ReferenceDetail />} />
       </Routes>
     </BrowserRouter>
   );
